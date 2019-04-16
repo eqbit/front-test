b77f91da27903fe36f6673448334c20f3daefdb1
eqbit <eqbits@gmail.com>
2019-04-16 11:06:36 +0500

    modification of the event form

--- src/components/AddEventForm/AddEventForm.js
+++ src/components/AddEventForm/AddEventForm.js
@@ -14,7 +14,7 @@ class AddEventForm extends React.Component {
             description: this.props.event && this.props.event.description ? this.props.event.description : ''
         }
     };
-
+    
     onClick = event => {
         event.stopPropagation();
     };
@@ -28,7 +28,7 @@ class AddEventForm extends React.Component {
     enableEdit = name => {
         this.setState({edit: name});
     };
-
+    
     render() {
         const {hasEvent, day, event} = this.props;
         const {edit} = this.state;
@@ -38,31 +38,92 @@ class AddEventForm extends React.Component {
                 <div className="event-form__close" onClick={this.props.handleFormClose}>×</div>
                 
                 {event && event.title && edit !== "title"
-                    ? <div className="event-form__event-title" title="редактировать"  onClick={() => this.enableEdit("title")}>{event.title}</div>
-                    : <input type="text" className="event-form_input" name="title" onChange={this.handleInputChange} placeholder='Событие' value={this.state.title}/>
+                    ?
+                    <div
+                        className="event-form__event-title"
+                        title="редактировать"
+                        onClick={() => this.enableEdit("title")}>
+                        {this.state.title ? this.state.title : event ? event.title : null}
+                    </div>
+                    :
+                    <input type="text"
+                           className="event-form_input" name="title"
+                           onChange={this.handleInputChange} placeholder='Событие'
+                           value={this.state.title}
+                    />
                 }
                 
-                {hasEvent && <div className="event-form__date">{format(day, "D")} {getMonthName(format(day, "M"))}</div>}
-    
-                {event && event.people && edit !== "people"
-                    ? <div className="event-form-event-people" title="редактировать"  onClick={() => this.enableEdit("people")}>
-                        <div className="eqvent-form-event-people__top">Участники:</div>
-                        <div className="eqvent-form-event-people__bottom">{event.people}</div>
-                    </div>
-                    : <input type="text" className="event-form_input" name="people" onChange={this.handleInputChange} placeholder='Имена участников' value={this.state.people}/>
+                {hasEvent &&
+                <div className="event-form__date">{format(day, "D")} {getMonthName(format(day, "M"))}</div>}
+                
+                {
+                    event && event.people && edit !== "people"
+                        ?
+                        <div className="event-form-event-people" title="редактировать"
+                             onClick={() => this.enableEdit("people")}>
+                            <div className="eqvent-form-event-people__top">Участники:</div>
+                            <div className="eqvent-form-event-people__bottom">
+                                {this.state.people ? this.state.people : event ? event.people : null}
+                            </div>
+                        </div>
+                        :
+                        <input
+                            type="text"
+                            className="event-form_input"
+                            name="people"
+                            onChange={this.handleInputChange}
+                            placeholder='Имена участников'
+                            value={this.state.people}
+                        />
                 }
-    
+                
                 {event && event.description && edit !== "description"
-                    ? <div className="event-form-event-description" title="редактировать"  onClick={() => this.enableEdit("description")}>
+                    ?
+                    <div
+                        className="event-form-event-description"
+                        title="редактировать"
+                        onClick={() => this.enableEdit("description")}
+                    >
                         <div className="eqvent-form-event-description__top">Подробно:</div>
                         <div className="eqvent-form-event-description__bottom">{event.description}</div>
                     </div>
-                    : <textarea className="event-form_input event-form_textarea" name="description" onChange={this.handleInputChange} placeholder="Описание" value={this.state.description}></textarea>
+                    :
+                    <textarea
+                        className="event-form_input event-form_textarea"
+                        name="description"
+                        onChange={this.handleInputChange}
+                        placeholder="Описание"
+                        value={this.state.description}
+                    > </textarea>
                 }
-
+                
                 <div className="event-form-buttons">
-                    {(edit || !hasEvent || !event.title || !event.people || !event.description) && <button onClick={() => this.props.submitHandler(day, this.state)}>Готово</button>}
-                    {hasEvent && <button onClick={() => this.props.deleteHandler(day)}>Удалить</button>}
+                    {
+                        (edit || !hasEvent || !event.title || !event.people || !event.description)
+                        &&
+                        <button
+                            onClick={() => {
+                                this.props.submitHandler(day, this.state);
+                                this.setState({edit: false});
+                            }}>
+                            Готово
+                        </button>
+                    }
+                    {
+                        hasEvent
+                        &&
+                        <button
+                            onClick={() => {
+                                this.props.deleteHandler(day);
+                                this.setState({
+                                    title: null,
+                                    people: null,
+                                    description: null
+                                })
+                            }}>
+                            Удалить
+                        </button>
+                    }
                 </div>
             </div>
         )
